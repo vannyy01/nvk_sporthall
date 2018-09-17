c0691e4eface9e449ea4ba9e6654c2510326eb3c
vannyy01 <vannyy01@gmail.com>
2018-09-17 17:43:15 +0300

    Build v1.0.2

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=nvk_sporthall',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=sql7.freemysqlhosting.net;dbname=sql7257227',
+    'username' => 'sql7257227',
+    'password' => 'cu84uvc7rj',
     'charset' => 'utf8',
 ];
